63045b4d4814558b988877e9e326ff0427f44a69
HomairaDaum <homaira_Daum@hotmail.com>
2026-07-23 15:07:06 +0100

    Update homepage for CI/CD test 1

--- app/server.js
+++ app/server.js
@@ -1110,7 +1110,7 @@ function createServer({ port = 3000, logger, metrics } = {}) {
 						<link rel="stylesheet" href="/styles.css" />
 					</head>
 					<body class="retro-shell">
-						<h1 class="retro-title">Tic Tac Toe</h1>
+						<h1 class="retro-title">Tic Tac Toe -ci/cd test 2:30</h1>
 						<div class="layout">
 							<div class="game-column">
 								<section class="panel">
